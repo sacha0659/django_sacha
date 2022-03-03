f8b8cb706ba40d0f780e8f74d34415d0d1edc532
sacha <sachagaleuzzi@gmail.com>
2022-03-03 11:42:21 +0100

    model product

--- app/models.py
+++ app/models.py
@@ -17,5 +17,13 @@ class Member(models.Model):
 	def __str__(self):
 		return self.name
 
+class Product(models.Model):
+    name = models.CharField('Nom',max_length=255)
+    description = models.CharField('Description',max_length=255)
+    prix = models.CharField('prix',max_length=20)
+    stock = models.IntegerField('stock',blank=True, default=0)
+    
+    def __str__(self) -> str:
+        return self.name
 
 # Create your models here.
