075313a2385837409f7625202818debf53d9089d
AhmedDWIIM <91135844+AhmedDWIIM@users.noreply.github.com>
2022-03-03 11:43:36 +0100

    fix url

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,7 @@ app_name = 'app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('add/', views.edit, name='add'),
+    path('chat/', views.chat, name='chat'),
     path('edit/<int:id>/', views.edit, name='edit'),
     path('detail/<int:id>/', views.detail, name='detail'),
     path('delete/<int:id>/', views.delete, name='delete'),
