2faa2014ab7f1b2db4da349ef9d7d338424c70b8
sacha <sachagaleuzzi@gmail.com>
2022-03-04 11:22:43 +0100

    form update product

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,5 @@
+from dataclasses import fields
+from pyexpat import model
 from django.forms import ModelForm
 from app.models import Member, Product
 
@@ -6,7 +8,13 @@ class MemberForm(ModelForm):
 	class Meta:
 		model = Member
 		fields = ('name','email','age',)
+        
 class ProductForm(ModelForm):
 	class Meta:
 		model = Product
 		fields = ('name','description','prix','stock','image')
+
+class ProductUpdateForm(ModelForm):
+    class Meta:
+        model = Product
+        fields = ('stock',)
